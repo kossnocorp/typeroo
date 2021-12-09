d9f97a0b9b191e42f90d66ab13f8fe0e8bedcb37
Sasha Koss <koss@nocorp.me>
2021-12-09 11:52:31 +0800

    Add Nullable

--- src/object/index.ts
+++ src/object/index.ts
@@ -6,6 +6,10 @@ export type OmitKeys<Type, ValueType> = {
   [Key in keyof Type]-?: Type[Key] extends ValueType ? never : Key;
 }[keyof Type];
 
+export type Nullable<Type> = {
+  [Key in keyof Type]: Type[Key] | null;
+};
+
 export type NullableKeys<Type, Keys extends keyof Type> = Omit<Type, Keys> & {
   [Key in Keys]: Type[Key] | null;
 };
