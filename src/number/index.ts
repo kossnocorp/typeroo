633a9507303d61d744592ae47f604944d6a0d341
Sasha Koss <koss@nocorp.me>
2023-04-19 14:44:01 +0800

    Add stringified number

--- src/number/index.ts
+++ src/number/index.ts
@@ -1,3 +1,19 @@
+import type { OpaqueString } from "../string";
+
 declare const opaqueNumberBrand: unique symbol;
 
 export type OpaqueNumber<Type> = number & { [opaqueNumberBrand]: Type };
+
+export type StringifiedNumber<Type> = OpaqueString<Type>;
+
+export function stringify<Type extends number>(
+  number: Type
+): StringifiedNumber<Type> {
+  return number.toString() as StringifiedNumber<Type>;
+}
+
+export function parse<Type extends number>(
+  number: StringifiedNumber<Type>
+): Type {
+  return parseFloat(number) as Type;
+}
